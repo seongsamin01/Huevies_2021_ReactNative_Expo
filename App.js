43c852241c9f5662d1e3cf50eb7f6b2bb8a90ca3
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-07 17:44:09 +0900

    cacheFonts
    폰트를 import 해서 넣어줬다

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
-import { Text } from "react-native";
+import * as Font from 'expo-font';
+import { Text, Image } from "react-native";
 import { Asset } from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
 
 const cacheImages = images => 
   images.map(image => {
@@ -12,13 +14,16 @@ const cacheImages = images =>
   }
 });
 
+const cacheFonts = fonts => fonts.map(font => [Font.loadAsync(font), Font.loadAsync(font)]);
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
       const images = cacheImages(["https://media.newyorker.com/photos/5f235208da874d6b001378a3/master/pass/Glasser-TrumpDelayTweet.jpg",
       require("./assets/splash.png")
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
   return isReady ? <Text>준비됐나?</Text> : (

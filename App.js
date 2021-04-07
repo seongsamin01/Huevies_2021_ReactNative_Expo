7c541b0e573c9bccb554b5b3cdc7f1aa332dde29
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-07 17:14:42 +0900

    CacheImages
    이미지 넣기

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import { Text } from "react-native";
+import { Asset } from 'expo-asset';
+
+const cacheImages = images => 
+  images.map(image => {
+  if(typeof image === "string"){
+    return Image.prefetch(image);
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = async () => {
+      const images = cacheImages(["https://media.newyorker.com/photos/5f235208da874d6b001378a3/master/pass/Glasser-TrumpDelayTweet.jpg",
+      require("./assets/splash.png")
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setIsReady(true);
+  return isReady ? <Text>준비됐나?</Text> : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish} 
+      onError={console.error} 
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+

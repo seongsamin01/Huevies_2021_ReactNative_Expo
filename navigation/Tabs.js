c4f9a0be12abd34396b4c8b53c9879a0e4e620d2
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-08 12:10:58 +0900

    Updating Header (UpTab) #2
    useLayoutEffect
    route
    getHeaderName 가지고
    UpTab Header 를 BottomTab Header와 맞춰줬음.

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -7,14 +7,21 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default ({ route }) => {
-    console.log(route);
+const getHeaderName = route => 
+    route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({ navigation, route }) => {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          title: getHeaderName(route)
+        });
+    }, [route]);
     return (
         <Tabs.Navigator>
             <Tabs.Screen name = "Movies" component={Movies} />
             <Tabs.Screen name = "Tv" component={Tv} />
             <Tabs.Screen name = "Search" component={Search} />
-            <Tabs.Screen name = "Favs" component={Favs} />
+            <Tabs.Screen name = "Favorites" component={Favs} />
         </Tabs.Navigator>
     );
 };

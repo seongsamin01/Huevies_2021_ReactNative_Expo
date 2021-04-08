b8ce07bf520cdbc4968778e46d751cbf3cb50079
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-08 17:57:05 +0900

    home slider 만들기 1
    npm i react-native-web-swiper
    Header
    Swiper
    Section
    Text

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,8 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { Dimensions } from "react-native";
+
+const { width, height } =Dimensions.get("screen")
+
+const Header = styled.View`
+    width: 100%;
+    height: ${height / 3}px;
+`;
+
+const Section = styled.View`
+    background-color: red;
+    height: 100%;
+`;
+
+const Text = styled.Text``;
 
 export default () => (
-    <View>
-        <Text>Presenter</Text>
-    </View>
+    <Header>
+        <Swiper>
+            <Section>
+                <Text>안녕</Text>
+            </Section>
+            <Section>
+                <Text>안녕</Text>
+            </Section>
+            <Section>
+                <Text>안녕</Text>
+            </Section>
+        </Swiper>
+    </Header>
 );

992b57790f22babdeb6d9a1d61337a3e22d6e7b2
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-08 14:03:26 +0900

    Navigation 스타일링 (BottomTab)
    IonIcons로
    movies, search,tv,favorites 아이콘넣어주고
    background black으로 맞춰주기

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
+import { Platform } from"react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -18,7 +20,36 @@ export default ({ navigation, route }) => {
         });
     }, [route]);
     return (
-        <Tabs.Navigator>
+        <Tabs.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused }) => {
+                    let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+                    if(route.name === "Movies"){
+                        iconName += "film";
+                    } else if(route.name === "Tv"){
+                        iconName += "tv";
+                    } else if(route.name === "Search"){
+                        iconName += "search";
+                    }else if(route.name === "Favorites"){
+                        iconName += "heart";
+                    }
+                    return (
+                        <Ionicons 
+                            name={iconName} 
+                            color={focused ? "white" : "grey"} 
+                            size={25} 
+                            />
+                        );
+                    }
+                })}
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    backgroundColor: "black",
+                    borderTopColof: "black"
+                }
+            }}
+        >
             <Tabs.Screen name = "Movies" component={Movies} />
             <Tabs.Screen name = "Tv" component={Tv} />
             <Tabs.Screen name = "Search" component={Search} />

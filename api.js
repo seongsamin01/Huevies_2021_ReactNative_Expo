a49fa78698209f185b54ac31544eed25c31cd3f7
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-08 15:01:14 +0900

    영화/TVshow API path (Layer)
    내가 원하는 영화/tvshow의 목록적기
    해당 목록마다 api path를 적어주기

---
+++ api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const TMDB_KEY = "ad8d0d181699c381f0e4ca436cb7149f"
+
+const makeRequest = (path, params) => 
+    axios.get(`https://api.themoviedb.org/3${path}`, {
+        params: {
+            ...params,
+            api_key: TMDB_KEY
+        }
+    });
+
+export const movieApi = {
+    nowPlaying: () => makeRequest("/movie/now_playing"),
+    popular: () => makeRequest("/movie/popular"),
+    upcoming: () => makeRequest("/movie/upcoming", { region= "kr"}),
+    search: query => makeRequest("/search/movie", { query }),
+    movie: id => makeRequest(`/movie/${id}`),
+    discover: () => makeRequest("/discover/movie")
+};
+
+export const tvApi = {
+    today: () => makeRequest("/tv/airing_today"),
+    thisWeek: () => makeRequest("/tv/on_the_air"),
+    topRated: () => makeRequest("/tv/top_rated"),
+    popular: () => makeRequest("/tv/popular"),
+    search: query => makeRequest("/search/tv", { query }),
+    show: id => makeRequest(`/tv/${id}`)
+};

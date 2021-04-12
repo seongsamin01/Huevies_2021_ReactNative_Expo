11989661d6c007e215de6f22b5677a7363a6c790
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-12 11:35:14 +0900

    Slide - Title, Votes, Overview 넣기
    Slide - Title, Votes, Overview 넣기

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,35 +1,41 @@
 import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-web-swiper";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, Dimensions, View } from "react-native";
 import Slide from "../../components/Movies/Slide";
 
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
+
 const Container = styled.View`
-    flex:1;
-    background-color:black;
+    flex: 1;
+    background-color: black;
     justify-content: center;
 `;
 
+const SliderContainer = styled.View`
+    width: ${WIDTH}px;
+    height: ${HEIGHT / 4}px;
+`;
 
 export default ({ loading, nowPlaying }) => (
   <Container>  
     {loading ? (
         <ActivityIndicator color="white" size="small" />
     ) : (
-        <>
+        <SliderContainer>
             <Swiper controlsEnabled={false} loop timeout={3}>
                {nowPlaying.map(movie => (
                    <Slide 
                       key={movie.id} 
                       id={movie.id} 
-                      title={movie.original_language}
+                      title={movie.original_title}
                       overview={movie.overview}
                       votes={movie.vote_average}
                       backgroundImage={movie.backdrop_path}
                     />
                 ))} 
             </Swiper>
-      </>
+      </SliderContainer>
     )}
   </Container>  
 );

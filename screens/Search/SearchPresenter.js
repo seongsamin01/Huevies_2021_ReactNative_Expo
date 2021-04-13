cf523c384a8ace9a1d79cf11704e039e207eac63
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-13 14:29:48 +0900

    Search - Styling 해주기
    margin
    padding
    border-radius

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,30 +1,51 @@
 import React from "react";
 import styled from "styled-components/native";
-import Horizontal from "../../components/Horizontal";
-import HorizontalSlider from "../../components/HorizontalSlider";
 import Input from "../../components/Search/Input";
+import HorizontalSlider from "../../components/HorizontalSlider";
+import Horizontal from "../../components/Horizontal";
+import Vertical from "../../components/Vertical";
 
 const Container = styled.ScrollView`
     background-color: black;
 `;
 
 export default ({ movies, shows, keyword, onChange, onSubmit }) => (
-    <Container>
-        <Input 
-            placeholder={"Write a keyword"}
-            value={keyword}
-            onChange={onChange}
-            onSubmit={onSubmit}
-        />
+    <Container 
+        contentContainerStyle={{
+            paddingTop: 10
+        }}
+    >
+    <Input
+        placeholder={"Write a keyword"}
+        value={keyword}
+        onChange={onChange}
+        onSubmit={onSubmit}
+    />
+    {movies.length !== 0 && (
         <HorizontalSlider title={"Movie Results"}>
             {movies.map(movie => (
-                <Horizontal />
+                <Vertical 
+                    key={movie.id}
+                    id={movie.id}
+                    votes={movie.vote_average}
+                    title={movie.title}
+                    poster={movie.poster_path || movie.backdrop_path}
+                />
             ))}
         </HorizontalSlider>
+    )}
+    {shows.length !== 0 && (
         <HorizontalSlider title={"TV Results"}>
             {shows.map(show => (
-                <Horizontal />
+                <Vertical 
+                    key={show.id}
+                    id={show.id}
+                    votes={show.vote_average}
+                    title={show.name}
+                    poster={show.poster_path}
+                />
             ))}
         </HorizontalSlider>
+    )}
     </Container>
 );
